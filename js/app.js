6d7c5bf1e146b1709aa2f84a71c343ea1e9bb309
chrissl8 <chrisjslaight@gmail.com>
2015-08-28 15:31:01 -0400

    Add functionality to remove items and update CSS styling for better look and feel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$('#add-item').submit(function(e){
 		e.preventDefault(); //Prevents refresh
 		var listValue = $('#list-value').val();
@@ -7,10 +8,17 @@ $(document).ready(function(){
 
 		}
 		else {
-		var listHTML = '<li>' +  listValue +  '</li>';
+		var listHTML = '<li><a class="remove-item">X</a> • ' +  listValue +  '</li>';
 		$('.shopping-list').prepend(listHTML); //Add list element
 		$('#list-value').val('');
 		$('#list-value').focus();
 		}
-	});
+	})
+	
+	$(document).on('click','.remove-item', function() {
+		console.log("Works!");	
+		 $(this).closest('li').remove();
+		 $('#list-value').focus();
+	})
+
 });
