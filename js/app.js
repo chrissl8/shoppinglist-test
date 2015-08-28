fc50a0842a52059919b9ddd3829a55582d23a066
chrissl8 <chrisjslaight@gmail.com>
2015-08-28 14:22:20 -0400

    Removed bug where empty input created new li elements using if-else logic

--- js/app.js
+++ js/app.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
 	$('#add-item').submit(function(e){
 		e.preventDefault(); //Prevents refresh
 		var listValue = $('#list-value').val();
+		if(listValue == new String(""))
+		{
+
+		}
+		else {
 		var listHTML = '<li>' +  listValue +  '</li>';
 		$('.shopping-list').prepend(listHTML); //Add list element
 		$('#list-value').val('');
+		}
 	});
 });
