65541cf216cc36574bcc57740ca3d9120ac5d2fc
chrissl8 <chrisjslaight@gmail.com>
2015-08-29 11:35:41 -0400

    Add full support for No Items To Show banner

--- js/app.js
+++ js/app.js
@@ -11,14 +11,41 @@ $(document).ready(function(){
 		var listHTML = '<li><a class="remove-item">X</a> • ' +  listValue +  '</li>';
 		$('.shopping-list').prepend(listHTML); //Add list element
 		$('#list-value').val('');
+		var listSize = $('.shopping-list li').length;
+		 if(listSize < 1)
+		 {
+			//$('.click-to-check').hide();
+			$('.no-items').show();
+		 }
+		 else
+		 {
+			$('.no-items').hide();
+			//$('.click-to-check').show();
+		 }
 		$('#list-value').focus();
 		}
 	})
 	
 	$(document).on('click','.remove-item', function() {
-		console.log("Works!");	
 		 $(this).closest('li').remove();
 		 $('#list-value').focus();
+		 var listSize = $('.shopping-list li').length;
+		 if(listSize < 1)
+		 {
+		 	//$('.click-to-check').hide();
+			$('.no-items').show();
+		 }
+		 else
+		 {
+			$('.no-items').hide();
+			//$('.click-to-check').show();
+		 }
 	})
 
+	$('.shopping-list li').click().toggleClass (
+    function(){$('.shopping-list li').css({'background-color': 'red'});},
+    function(){$('.shopping-list li').css({'background-color': '#FF99CC'});
+})
+
 });
+
